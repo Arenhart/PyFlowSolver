6483c18e4dd814c984d84805da95377fcabdf419
Rafael Arenhart <rafael.arenhart@gmail.com>
2024-01-22 23:03:28 -0300

    direct sparse extractor

--- tests/unit/test_volume_manager.py
+++ tests/unit/test_volume_manager.py
@@ -22,10 +22,16 @@ def test_get_linear_system():
     np.testing.assert_array_equal(A, A_template)
     np.testing.assert_array_equal(b, b_template)
 
-def test_get_sparse_system():
+def test_get_indirect_sparse_system():
     A, b = volume_manager.get_linear_system()
-    sparse_array = volume_manager.get_sparse_system(A, b)
+    sparse_array, condensed_b = volume_manager.get_sparse_system_from_dense(A, b)
     np.testing.assert_array_equal(sparse_array["val"], sparse_array_template["val"])
+    np.testing.assert_array_equal(sparse_array["col_idx"], sparse_array_template["col_idx"])
+    np.testing.assert_array_equal(sparse_array["row_ptr"], sparse_array_template["row_ptr"])
+
+def test_get_sparse_system():
+    sparse_array, condensed_b = volume_manager.get_sparse_system()
+    np.testing.assert_allclose(sparse_array["val"], sparse_array_template["val"], rtol=1e-06)
     np.testing.assert_array_equal(sparse_array["col_idx"], sparse_array_template["col_idx"])
     np.testing.assert_array_equal(sparse_array["row_ptr"], sparse_array_template["row_ptr"])
 
