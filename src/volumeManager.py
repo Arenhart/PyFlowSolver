04cb8429f33d8f073eb1d1ca3bbbabf5c049b8ef
Rafael Arenhart <rafael.arenhart@gmail.com>
2024-01-22 23:03:28 -0300

    direct sparse extractor

--- src/volumeManager.py
+++ src/volumeManager.py
@@ -4,6 +4,7 @@ class VolumeManager():
 
     def __init__(self, volume):
         self.volume = volume
+        self.nulls_count = None
         self._generate_neighbours_dict()
 
     def _generate_neighbours_dict(self):
@@ -74,7 +75,7 @@ class VolumeManager():
 
         return A, b
     
-    def get_sparse_system(self, A, b):
+    def get_sparse_system_from_dense(self, A, b):
 
         diag = np.diagonal(A)
         diag_nulls_count = np.zeros(diag.shape, dtype=int)
@@ -115,4 +116,105 @@ class VolumeManager():
             "row_ptr": row_ptr_array,
         }
 
-        return sparse_array
+        return sparse_array, condensed_b
+    
+    def get_sparse_system(self):
+        nonzeros = np.count_nonzero(self.volume)
+        self.nulls_count = np.empty(self.volume.size, dtype=int)
+        running_zeros = 0
+        i = 0
+        w, h, d = self.volume.shape
+        for x, y, z in ((a,b,c) for a in range(w) for b in range(h) for c in range(d)):
+            center_c = self.volume[x, y, z]
+            if center_c > 0:
+                self.nulls_count[i] = running_zeros
+                i += 1
+            else:
+                running_zeros += 1
+                self.nulls_count[i] = running_zeros
+                i += 1
+        
+        val_array = np.zeros(nonzeros * 7)
+        col_idx_array = np.zeros(nonzeros * 7, dtype=int)
+        row_ptr_array = np.zeros(nonzeros, dtype=int)
+        condensed_b = np.zeros(nonzeros)
+        
+        sparse_i = 0
+        vals_n = 0
+
+        for x, y, z in ((a,b,c) for a in range(w) for b in range(h) for c in range(d)):
+            coords = np.array((x, y, z))
+            center_c = self.volume[tuple(coords)]
+
+            if center_c == 0:
+                continue
+
+            x_min = (x == 0)
+            x_max = (x == w - 1)
+            y_min = (y == 0)
+            y_max = (y == h - 1)
+            z_min = (z == 0)
+            z_max = (z == d - 1)
+
+            key = (x_min, x_max, y_min, y_max, z_min, z_max)
+            neighbours = self.neighbours_dict[key]
+
+            total_c = np.float32(0)
+
+            if z_min:
+                total_c += 2 * center_c
+                condensed_b[self._unravel(x, y, z)] = -(2 * center_c)
+            elif z_max:
+                total_c += 2 * center_c
+
+            center_i = self._unravel(x, y, z)
+            row_ptr_array[center_i] = vals_n
+            for neighbour in neighbours:
+                neighbour_c = self.volume[tuple(coords + neighbour)]
+                if neighbour_c == 0: continue
+                face_c = np.float32(2 / (1 / center_c + 1 / neighbour_c))
+                total_c += np.float32(face_c)
+                neighbour_i = self._unravel(*tuple(coords + neighbour))
+                val_array[vals_n] = face_c
+                col_idx_array[vals_n] = neighbour_i
+                vals_n += 1
+            val_array[vals_n] = -total_c
+            col_idx_array[vals_n] = self._unravel(*tuple(coords))
+            vals_n += 1
+            i += 1
+
+        val_array.resize(vals_n)
+        col_idx_array.resize(vals_n)
+
+        sparse_array = {
+        "val": val_array,
+        "col_idx": col_idx_array,
+        "row_ptr": row_ptr_array,
+        }
+
+        self._sort_sparse_array(sparse_array)
+
+        return sparse_array, condensed_b
+
+    def _unravel(self, x, y, z):
+        _, h, d = self.volume.shape
+        i = z + y * d + x * d * h
+
+        output = i - self.nulls_count[i]
+
+        return output
+    
+    def _sort_sparse_array(self, sparse_array):
+        for start, stop in self.row_indexes_iterator(sparse_array):
+            val = sparse_array["val"][start:stop]
+            col_idx = sparse_array["col_idx"][start:stop]
+            col_idx, val = zip(*sorted(zip(col_idx, val)))
+            sparse_array["val"][start:stop] = val
+            sparse_array["col_idx"][start:stop] = col_idx
+
+
+    def row_indexes_iterator(self, sparse_array):
+        for start, stop in zip(sparse_array["row_ptr"][:-1], sparse_array["row_ptr"][1:]):
+            yield start, stop
+        else:
+            yield sparse_array["row_ptr"][-1], sparse_array["val"].size
