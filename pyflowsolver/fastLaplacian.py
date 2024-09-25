c8ffe357e6584585b107a17dc54e0c98a3873c29
Rafael Arenhart <rafael.arenhart@ltrace.com.br>
2024-09-25 17:32:05 -0300

    closed border boundary condition

--- pyflowsolver/fastLaplacian.py
+++ pyflowsolver/fastLaplacian.py
@@ -22,8 +22,12 @@ def fast_laplacian_volume_generator(porosity_volume, pore_scale, subresolution_f
     darcy_pores = porosity_volume * darcy_pores
     darcy_pores = subresolution_function(darcy_pores)
 
-    conductance_array = np.zeros_like(porosity_volume, dtype=np.float32)
-    conductance_array = edt(stokes_pores, scale=pore_scale, force_method="cpu")
+    #conductance_array = np.zeros_like(porosity_volume, dtype=np.float32)
+    w, h, d = stokes_pores.shape
+    stokes_pores_with_border = np.zeros((w+2, h+2, d), dtype=stokes_pores.dtype)
+    stokes_pores_with_border[1:-1, 1:-1, :] = stokes_pores
+    conductance_array = edt(stokes_pores_with_border, scale=pore_scale, force_method="cpu")
+    conductance_array = conductance_array[1:-1, 1:-1, :]
     alfa = np.min(pore_scale)/2
     conductance_array = (conductance_array - alfa)**2
     conductance_array *= stokes_pores
